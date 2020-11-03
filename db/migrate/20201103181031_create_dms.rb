59cfca2bd0e1bb5a30ddb868a4c69ec9fa4ab190
SophiaGrace16 <sophia.g.white@outlook.com>
2020-11-03 13:12:27 -0500

    adding DM model

---
+++ db/migrate/20201103181031_create_dms.rb
@@ -0,0 +1,11 @@
+class CreateDms < ActiveRecord::Migration[6.0]
+  def change
+    create_table :dms do |t|
+      t.string :username
+      t.string :name
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
