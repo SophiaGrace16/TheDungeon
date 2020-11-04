3ca3ebe88fd84af7f68adbf976645c7cae5672f5
SophiaGrace16 <sophia.g.white@outlook.com>
2020-11-04 08:35:41 -0500

    editing games table, before first migration

--- db/migrate/20201103200730_create_games.rb
+++ db/migrate/20201103200730_create_games.rb
@@ -2,9 +2,12 @@ class CreateGames < ActiveRecord::Migration[6.0]
   def change
     create_table :games do |t|
       t.boolean :active
-      t.integer :story_id
+      t.integer :dm_id
       t.integer :player_id
       t.string :camp_name
+      t.text :loot_found
+      t.integer :exp_gained
+      t.date :date_started
 
       t.timestamps
     end
