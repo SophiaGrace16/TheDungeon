802fd055fd0381840c33cff4cfa8f806e36111c1
SophiaGrace16 <sophia.g.white@outlook.com>
2020-11-04 16:20:55 -0500

    added helpers for logged_in and current_

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    helper_method :player_logged_in?
+    helper_method :current_player
+    helper_method :dm_logged_in?
+    helper_method :current_dm
+
+    def current_player
+        @player ||= Player.find_by_id(session[:player_id])
+      end
+    
+    def player_logged_in?
+        !current_player.nil?
+    end
+
+    def current_dm
+        @dm ||= Dm.find_by_id(session[:dm_id])
+      end
+    
+    def dm_logged_in?
+        !current_dm.nil?
+    end
+    
 end
