50c1a6b693760ec056025453bfd92e879d4d7b6a
SophiaGrace16 <sophia.g.white@outlook.com>
2020-11-04 08:37:58 -0500

    adding associations to models

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,3 @@
 class Character < ApplicationRecord
     belongs_to :player
-    # belongs_to :game
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,3 @@
 class Story < ApplicationRecord
     belongs_to :dm
-    # belongs_to :game
 end
