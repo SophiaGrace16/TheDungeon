d426d7e7765a82d28a3bccd74a745082ec9befa1
SophiaGrace16 <sophia.g.white@outlook.com>
2020-11-05 17:16:09 -0500

    adding character/story validations & filling out character views

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,3 +1,5 @@
 class Story < ApplicationRecord
     belongs_to :dm
+
+    validates :title, :setting, :camp_type, :final_boss, :item_reward, :coin_reward, :exp_reward, :story_link, :summary, :difficulty, presence: true
 end
