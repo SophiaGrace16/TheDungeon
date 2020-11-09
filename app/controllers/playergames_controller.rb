f168d0d8270569611a346b89720989a429b127ac
SophiaGrace16 <sophia.g.white@outlook.com>
2020-11-09 09:17:13 -0500

    adding playergames views, validations, and errors

---
+++ app/controllers/playergames_controller.rb
@@ -0,0 +1,74 @@
+class PlayergamesController < ApplicationController
+    before_action :set_game, only: [:show, :edit, :update, :destroy]
+    before_action :set_player
+    before_action :redirect_if_player_not_logged_in
+
+    def index
+        @playergames = Playergame.all
+    end
+
+    def new 
+        # gets new form
+        @playergame = Playergame.new
+        @dm=Dm.all
+    end
+
+    def create
+        #posts new form
+        @playergame = current_player.playergames.build(playergame_params) #sets this to the current player
+        # binding.pry
+        if @playergame.save
+            redirect_to player_playergame_path(@player,@playergame)
+        else
+            render 'new_player_playergame'
+        end
+    end
+
+    def edit
+        #gets edit form
+        if !@playergame
+            redirect_to player_playergames_path(@player)
+        end
+    end
+
+    def show
+        #shows the information
+        if !@playergame
+            redirect_to player_playergames_path(@player)
+        end
+    end
+
+    def update
+    #posts edit form
+        if @playergame
+            @playergame.update(playergame_params)
+                if @playergame.errors.any?
+                    render 'edit'
+                else
+                    redirect_to player_playergame_path(@player,@playergame)
+                end
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        #deletes the game
+        @playergame.destroy
+        redirect_to player_playergames_path(@player)
+    end
+
+private
+
+    def set_player
+        @player = Player.find_by_id(params[:player_id])
+    end
+
+    def set_game
+        @playergame = Playergame.find_by_id(params[:id])
+    end
+
+    def playergame_params
+        params.require(:playergame).permit(:camp_name, :loot_found, :character, :date_joined, :coin_reward, :exp_gained, :game_notes, :player_host_name)
+    end
+end
--- app/models/playergame.rb
+++ app/models/playergame.rb
@@ -2,6 +2,6 @@ class Playergame < ApplicationRecord
     belongs_to :player
     belongs_to :game
 
-    validates :camp_name, :loot_found, :coin_reward, :exp_reward, :game_notes, :dm_host_name, presence: true
+    validates :camp_name, :loot_found, :character, :date_joined, :coin_reward, :exp_gained, :game_notes, :dm_host_name, presence: true
 
 end
